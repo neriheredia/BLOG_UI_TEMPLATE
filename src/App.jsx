3a8e4a6935ccd51d87547392cbc3641cb8ff6e08
neriheredia <developer.heredia@gmail.com>
2022-12-19 07:50:17 -0300

    feat: create template

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+	return (
+		<div
+			style={{
+				alignItems: 'center',
+				background: 'gray',
+				display: 'flex',
+				height: '100vh',
+				justifyContent: 'center',
+			}}
+		>
+			<h1
+				style={{
+					color: 'white',
+					fontSize: '3rem',
+				}}
+			>
+				Bienvenidos al grupo de estudio!
+			</h1>
+		</div>
+	);
+};
+
+export default App;
