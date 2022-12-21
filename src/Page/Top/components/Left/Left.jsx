d951b8fc7717341f96e396925c493aa6bbb5a2c2
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-21 17:42:50 -0300

    todo listo

--- src/Page/Top/components/Left/Left.jsx
+++ src/Page/Top/components/Left/Left.jsx
@@ -4,10 +4,15 @@ import styles from './Left.module.css';
 function Left() {
 	return (
 		<div className={styles.left}>
-			<NavLink to='' className='fa-brands fa-square-facebook'></NavLink>
+			{/* <NavLink to='' className='fa-brands fa-square-facebook'></NavLink>
 			<NavLink to='' className='fa-brands fa-square-instagram'></NavLink>
 			<NavLink to='' className='fa-brands fa-square-facebook'></NavLink>
-			<NavLink to='' className='fa-brands fa-square-twitter'></NavLink>
+			<NavLink to='' className='fa-brands fa-square-twitter'></NavLink> */}
+
+			<NavLink to='' className='topIcon fab fa-facebook-square'></NavLink>
+			<NavLink to='' className='topIcon fab fa-instagram-square'></NavLink>
+			<NavLink to='' className='topIcon fab fa-pinterest-square'></NavLink>
+			<NavLink to='' className='topIcon fab fa-twitter-square'></NavLink>
 		</div>
 	);
 }
