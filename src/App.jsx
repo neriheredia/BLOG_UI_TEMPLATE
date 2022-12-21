d6f4a7a909a73b4f8b6ba5a1f4025996ad318e7b
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-21 16:16:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,  } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './Page/Header/Header';
 import Home from './Page/Home/Home';
 import Top from './Page/Top/Top';
