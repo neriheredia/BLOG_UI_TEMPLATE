b1cfca0f0e5717771b9cac99fc0014f094d132cb
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-22 15:19:01 -0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 		<BrowserRouter>
 			<Navbar />
 			<Routes>
-				<Route path='/' element={<Home />}/>
+				<Route path='/' element={<Home />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import Header from "../../Components/Header/Header";
+import Header from '../../Components/Header/Header';
 
 function Home() {
-	return <div>
-		<Header/>
-	</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 }
 export default Home;
