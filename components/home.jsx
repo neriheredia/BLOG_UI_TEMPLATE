1fde2d366626cd18ac7fe08d02165518914148f2
Joel Chasmann <joelchasmann@gmail.com>
2022-12-27 12:44:43 -0300

    feat: agrego navbar

---
+++ components/home.jsx
@@ -0,0 +1,6 @@
+import '../styles/home.css';
+const Home = props => {
+	return <div></div>;
+};
+
+export default Home;
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
+import Nav from '../components/nav';
+import Home from '../components/home';
 const App = () => {
 	return (
-		<div
-			style={{
-				alignItems: 'center',
-				background: 'red',
-				display: 'flex',
-				height: '100vh',
-				justifyContent: 'center',
-			}}
-		>
-			<h1
-				style={{
-					color: 'white',
-					fontSize: '3rem',
-				}}
-			>
-				Bienvenidos al grupo de estudio perri!
-			</h1>
-		</div>
+		<>
+			<Nav />
+			<Home />
+		</>
 	);
 };
 
