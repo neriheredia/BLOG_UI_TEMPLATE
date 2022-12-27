f9fc8b7752b772d6c98ec6b49c96815f5623f7f4
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-27 13:08:18 -0300

    new

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import img from '../../assets/foto6.webp';
-import "./Header.css"
+import './Header.css';
 
 function Header() {
 	return (
 		<div className='header'>
-			<div className="titles">
+			<div className='titles'>
 				<span className='reactNode'>React & Node</span>
 				<span className='blog'>BLOG</span>
 			</div>
-			<img className='img' src={img} alt='imgPortada'/>
+			<img className='img' src={img} alt='imgPortada' />
 		</div>
 	);
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,5 @@
-import "./Login.css"
+import './Login.css';
 function Login() {
-  return (
-    <div>Login</div>
-  )
+	return <div>Login</div>;
 }
-export default Login
+export default Login;
