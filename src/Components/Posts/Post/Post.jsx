adad2ad404cfc033ccce824f8c7caf512c37e641
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-28 16:10:28 -0300

    ok

--- src/Components/Posts/Post/Post.jsx
+++ src/Components/Posts/Post/Post.jsx
@@ -1,18 +1,26 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import './Post.css';
 
 function Post(props) {
 	return (
-		<div className="post">
-			<img src={props.image} alt="img" /><br />
-			<div className="info">
-				<div className="div">
-					{props.category?.map((c,key)=>(<span key={key} className='category'>{c}</span>))}
+		<div className='post'>
+			<img src={props.image} alt='img' />
+			<br />
+			<div className='info'>
+				<div className='div'>
+					{props.category?.map((c, key) => (
+						<span key={key} className='category'>
+							{c}
+						</span>
+					))}
 				</div>
-				<NavLink className='title' to=''>{props.title}</NavLink><br />
-				<span className="time">{props.time}</span>
+				<NavLink className='title' to=''>
+					{props.title}
+				</NavLink>
+				<br />
+				<span className='time'>{props.time}</span>
 			</div>
-			<p className="text">{props.overview}</p>
+			<p className='text'>{props.overview}</p>
 		</div>
 	);
 }
--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,13 +1,13 @@
 import Post from './Post/Post';
-import post from "../../constants/posts.json";
+import post from '../../constants/posts.json';
 import './Posts.css';
 
 function Posts() {
 	return (
 		<div className='posts'>
-			{post.map((post,key) => (
-				<Post 
-					key={key} 
+			{post.map((post, key) => (
+				<Post
+					key={key}
 					image={post.image}
 					category={post.category}
 					overview={post.overview}
