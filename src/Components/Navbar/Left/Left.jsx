f785b69e638c900f09478c1df6d9e3ccc2702c0a
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-01-03 10:40:43 -0300

    build(login): login page and your css style

--- src/Components/Navbar/Left/Left.jsx
+++ src/Components/Navbar/Left/Left.jsx
@@ -4,10 +4,22 @@ import './Left.css';
 function Left() {
 	return (
 		<div className='left'>
-			<NavLink to='' className='topIcon fab fa-facebook-square leftcolor'></NavLink>
-			<NavLink to='' className='topIcon fab fa-instagram-square leftcolor'></NavLink>
-			<NavLink to='' className='topIcon fab fa-pinterest-square leftcolor'></NavLink>
-			<NavLink to='' className='topIcon fab fa-twitter-square leftcolor'></NavLink>
+			<NavLink
+				to=''
+				className='topIcon fab fa-facebook-square leftcolor'
+			></NavLink>
+			<NavLink
+				to=''
+				className='topIcon fab fa-instagram-square leftcolor'
+			></NavLink>
+			<NavLink
+				to=''
+				className='topIcon fab fa-pinterest-square leftcolor'
+			></NavLink>
+			<NavLink
+				to=''
+				className='topIcon fab fa-twitter-square leftcolor'
+			></NavLink>
 		</div>
 	);
 }
--- src/Components/Navbar/Right/Right.jsx
+++ src/Components/Navbar/Right/Right.jsx
@@ -6,7 +6,10 @@ function Rigth() {
 		<div className='right'>
 			<NavLink to='/login'>LOGIN</NavLink>
 			<NavLink to='/register'>REGISTER</NavLink>
-			<NavLink to='' className='topSearchIcon fas fa-search rigthcolor'></NavLink>
+			<NavLink
+				to=''
+				className='topSearchIcon fas fa-search rigthcolor'
+			></NavLink>
 		</div>
 	);
 }
